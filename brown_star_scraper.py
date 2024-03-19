1b92146116d51fbd7f8c8447009d12579061e427
QuiggleSams <87545645+shloksheth@users.noreply.github.com>
2024-03-19 16:04:09 -0400

    Add files via upload

--- brown_star_scraper.py
+++ brown_star_scraper.py
@@ -5,7 +5,7 @@ import time
 
 driver = webdriver.Chrome()
 driver.get("https://en.wikipedia.org/wiki/List_of_brown_dwarfs")
-time.sleep(10)
+time.sleep(40)
 # html = driver.page_source
 # soup = BeautifulSoup(html, "html.parser")
 scraped_data = []
@@ -16,8 +16,8 @@ brown_star_table = soup.find_all("table", {"class":"wikitable sortable jquery-ta
 temp_list = []
 table_rows = brown_star_table[1].find_all('tr')
 
-for i in table_rows:
-    td = i.find_all('td')
+for tr in table_rows:
+    td = tr.find_all('td')
     row = [i.text.rstrip() for i in td]
     temp_list.append(row)
     
@@ -27,18 +27,24 @@ mass = []
 radius = []
 
 print(temp_list)
-stars_data = []
+# stars_data = []
 
 for r in range(1, len(temp_list)):
 
     star_names.append(temp_list[r][0])
     distance.append(temp_list[r][5])
-    mass.append(temp_list[r][7])
-    radius.append(temp_list[r][8])
+    mass.append(temp_list[r][8])
+    radius.append(temp_list[r][9])
     
-    required_data = [star_names, distance, mass, radius]
-    stars_data.append(required_data) 
+    # required_data = [star_names, distance, mass, radius]
+    # stars_data.append(required_data) 
 
-headers =  ['Star_Name', 'Distance', 'Mass', 'Radius']
-df2 = pd.DataFrame(stars_data, columns=headers)
+# headers =  ['Star_Name', 'Distance', 'Mass', 'Radius']
+# df2 = pd.DataFrame(stars_data, columns=headers)
+# df2.to_csv('brown_dwarf_stars.csv', index=True, index_label="id")
+
+headers = ['Star_Name','Distance','Mass','Radius']  
+df2 = pd.DataFrame(list(zip(star_names,distance,mass,radius,)),columns=['Star_Name','Distance','Mass','Radius'])
+print(df2)
+
 df2.to_csv('brown_dwarf_stars.csv', index=True, index_label="id")
